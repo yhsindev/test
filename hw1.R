46cf9dbbc9276e69cb0fdd115d8b0c779419db96
黃妍心 <huangyanxin@IRISMacLIFE.local>
2024-03-11 15:20:48 +0800

    7th commit

--- hw1.R
+++ hw1.R
@@ -3,5 +3,5 @@ summary <- function(input_path, output_path) {
   weight_max <- round(max(data$weight),2)
   height_max <- round(max(data$height),2)
   output <- data.frame(set=input_path, weight = weight_max, height = height_max)
-  write.csv(output, file=output_path, row.names = FALSE, col.names = TRUE)
+  write.csv(output, file=output_path, row.names = FALSE)
 }
