0d41aa15f2dc93c7f0948bcf8060a78021c9af1f
黃妍心 <huangyanxin@IRISMacLIFE.local>
2024-03-11 15:51:08 +0800

    9th commit

--- hw1.R
+++ hw1.R
@@ -2,8 +2,6 @@ summary <- function(input_path, output_path) {
   data <- read.csv(input_path)
   weight_max <- round(max(data$weight),2)
   height_max <- round(max(data$height),2)
-  output <- data.frame(set=input_path, weight = weight_max, height = height_max)
+  output <- data.frame(set=basename(input_path), weight = weight_max, height = height_max)
   write.csv(output, file=output_path, row.names = FALSE)
 }
-
-summary
