765e3d6196166aba679a4d33b7a05595d9f70351
黃妍馨 <huangyanxin@IRISMacLIFE.local>
2024-03-11 13:22:54 +0800

    1st commit

---
+++ hw1.R
@@ -0,0 +1,8 @@
+summary <- function(input_path, output_path) {
+  library(math)
+  data <- read.csv('input.csv')
+  weight_max <- round(max(data$weight),2)
+  height_max <- round(max(data$height),2)
+  output <- data.frame(set="input.csv", weight = weight_max, height = height_max)
+  write.csv(output, file="output.csv", col.names = TRUE)
+}
