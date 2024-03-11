2672b8bd4b4b88934333177a530a5ee6a6a4cdfe
黃妍心 <huangyanxin@IRISMacLIFE.local>
2024-03-11 14:25:55 +0800

    2nd commit

--- hw1.R
+++ hw1.R
@@ -6,3 +6,5 @@ summary <- function(input_path, output_path) {
   output <- data.frame(set="input.csv", weight = weight_max, height = height_max)
   write.csv(output, file="output.csv", col.names = TRUE)
 }
+
+summary
